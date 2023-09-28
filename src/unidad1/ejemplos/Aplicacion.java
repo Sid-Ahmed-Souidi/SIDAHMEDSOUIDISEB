57c3c37226355185d97dc57ae7685656946b2343
ciclofp <ciclofp@10.0.2.15>
2023-09-28 14:18:17 +0200

    PRIMER COMMIT

---
+++ src/unidad1/ejemplos/Aplicacion.java
@@ -0,0 +1,14 @@
+package unidad1.ejemplos;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		// comentario
+		
+		/**
+		 * Este es mi primer comentario en el programa 
+		 */
+		System.out.println("Empezamos a programar en java ");
+	}
+
+}
