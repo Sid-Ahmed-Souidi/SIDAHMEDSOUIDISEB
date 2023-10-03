8808f1622dc4c545a0b3cb675c09159d33c450f5
ciclofp <ciclofp@10.0.2.15>
2023-10-03 12:05:39 +0200

    comentario

--- src/unidad1/ejemplos/Aplicacion.java
+++ src/unidad1/ejemplos/Aplicacion.java
@@ -9,7 +9,9 @@ public class Aplicacion {
 		 * Este es el programa para iniciar 
 		 */
 		System.out.println("Empezamos a programar en java ");
-		System.out.println("Ciclo desarrollo de aplicaciones web");
+		System.out.println("Ciclo: desarrollo de aplicaciones web");
+		System.out.println("Tercera prueba");
+
 	}
 	
       
