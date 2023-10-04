8238d40c8c4c19bb0028ce1ecdbfeb9adf4ca6f5
Sid Ahmed Souidi <Sid Ahmed Souidi@DESKTOP-LDML3OF>
2023-10-04 19:03:53 +0200

    ejercicio1y2

---
+++ src/unidad2/ejemplos/Ejercicio2.java
@@ -0,0 +1,28 @@
+package unidad2.ejemplos;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+	//	a) la cantidad de hermanos que tienes.
+		int cantidadHermanos = 6;
+	//	b) tu nota final en esta clase.
+		int notaFinal = 7;
+    //	c) la población de la tierra.
+		int poblacionTotal = 7000000000;
+	//	d) la población de una provincia de España.
+		int PoblacionProvincia = 200.000;
+	//	e) la cantidad de pasajeros en un autobús.
+		int cantidadPasajeros = 34;
+	//	f)  la puntuación de un jugador en un juego de Baloncesto.
+		int totalPuntos = 130;
+	//	g) El precio de un artículo de un supermercado.
+		float precioArticulo = 12.4;
+	//	h) El año en que ocurrió un evento histórico.
+		String fechaEvento = "12/02/2012";
+	//	i)  La cantidad de patas en un animal.
+		int patasAnimal = 4;
+	//	j)  El precio de un automóvil.
+		float precioCoche = 20000; 
+	}
+
+}
