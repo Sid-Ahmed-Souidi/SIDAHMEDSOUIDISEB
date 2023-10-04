60af0ec7c7e0256d7827d4edf342b9d4ff22db5e
ciclofp <ciclofp@10.0.2.15>
2023-10-04 10:01:28 +0200

    unidad2ejemplos

---
+++ src/unidad2/ejemplos/VariablesyTipos.java
@@ -0,0 +1,41 @@
+package unidad2.ejemplos;
+
+public class VariablesyTipos {
+
+	public static void main(String[] args) {
+		//nombre de variables y tipos de datos
+		
+		//DATOS DEL PROPIETARIO
+		String dni="98739453H"; //Almacena el numero+letra
+		System.out.println("Dni:"+dni);
+		System.out.println("Edad");
+		String nombrePropietario;
+		String direccion; 
+		String telefono; // +34 ..
+		String cp;
+		//DATOS DEL ANIMAL 
+		long id;
+		String nombreAnimal;
+		char sexo;
+		int edad;
+		edad = 12;
+		System.out.println("edad:"+edad);
+		String raza;
+		String enfermedad;
+		boolean estaVacunado;
+		float peso;
+		boolean estaCastrado;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
