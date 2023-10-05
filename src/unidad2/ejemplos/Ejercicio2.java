6b33aba94170a05a467dbcc1fd23230612af0233
ciclofp <ciclofp@10.0.2.15>
2023-10-05 14:31:14 +0200

    comuit

--- src/unidad2/ejemplos/Ejercicio2.java
+++ src/unidad2/ejemplos/Ejercicio2.java
@@ -4,25 +4,25 @@ public class Ejercicio2 {
 
 	public static void main(String[] args) {
 	//	a) la cantidad de hermanos que tienes.
-		int cantidadHermanos = 6;
+	//	int cantidadHermanos = 6;
 	//	b) tu nota final en esta clase.
-		int notaFinal = 7;
+	//	int notaFinal = 7;
     //	c) la población de la tierra.
-		int poblacionTotal = 7000000000;
+	//	int poblacionTotal = 7000000000;
 	//	d) la población de una provincia de España.
-		int PoblacionProvincia = 200.000;
+	//	int PoblacionProvincia = 200.000;
 	//	e) la cantidad de pasajeros en un autobús.
-		int cantidadPasajeros = 34;
+	//	int cantidadPasajeros = 34;
 	//	f)  la puntuación de un jugador en un juego de Baloncesto.
-		int totalPuntos = 130;
+	//	int totalPuntos = 130;
 	//	g) El precio de un artículo de un supermercado.
-		float precioArticulo = 12.4;
+	//	float precioArticulo = 12.4;
 	//	h) El año en que ocurrió un evento histórico.
-		String fechaEvento = "12/02/2012";
+	//	String fechaEvento = "12/02/2012";
 	//	i)  La cantidad de patas en un animal.
-		int patasAnimal = 4;
+	//	int patasAnimal = 4;
 	//	j)  El precio de un automóvil.
-		float precioCoche = 20000; 
+	//	float precioCoche = 20000; 
 	}
 
 }
