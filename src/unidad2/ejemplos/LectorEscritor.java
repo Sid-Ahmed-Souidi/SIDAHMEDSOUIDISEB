2ec1a02556b74c02306edcbff78ddb48389bb90f
ciclofp <ciclofp@10.0.2.15>
2023-10-06 09:34:02 +0200

    cojeihpdiu

--- src/unidad2/ejemplos/LectorEscritor.java
+++ src/unidad2/ejemplos/LectorEscritor.java
@@ -9,24 +9,34 @@ public class LectorEscritor {
 		
 		int dato;
 		int dato2;
+		int bancario;
+		String nombre;
+		Scanner entrada = new Scanner(System.in);// entrada es un objeto 
 
+		
+		System.out.println("Introduzca su nombre: ");
+		
+		nombre = entrada.nextLine(); // nextLine permite escribir nombres compuestos con espacio
+		//nombre = entrada.next(); solo sirve para una palabra 
+		
 		System.out.print("Dame un número entero");
-		Scanner entrada = new Scanner(System.in);
 		
 		dato = entrada.nextInt();
 		
-		System.out.println("Usted ha introducido el numero:"+dato);
+		System.out.println(nombre+"Ha introducido el numero:"+dato);
 		
-		System.out.println("Dame otro");
+		System.out.println("Dame otro"+nombre+":");
 		dato2 = entrada.nextInt();
 		System.out.println("Y ahora:"+dato2);
+		
 		
 		int suma = dato+dato2;
 		
 		System.out.println("La suma es:"+suma);
 		System.out.println("La suma es:"+dato+dato2);//de esta forma lo que hace es concatenar y no sumar.
 		System.out.println("La suma es:"+(dato+dato2));//Para hacer la suma en el print hay que ponerlo entre parentesis.
-
+		
+		entrada.nextLine();//el no poner indicador sirve para vaciar el buffer 
 		
 
 		
---
+++ src/unidad2/ejemplos/Literales.java
@@ -0,0 +1,24 @@
+package unidad2.ejemplos;
+
+public class Literales {
+
+	public static void main(String[] args) {
+		
+		int b=0b00000101_00111001_01111111_10110001;
+		
+		System.out.println(b);
+		
+		int c=0b0000_0101_0011_1001_0111_1111_1011_0001;//la b indica que viene un numero binario 
+		
+		int d=0x5___3___9___7___F___B___1; //esta en numero hexadecimal y se indica con 0x
+		
+		int e =00100; //el cero del principio indica que es un numero octal 
+		
+		
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		
+	}
+
+}
