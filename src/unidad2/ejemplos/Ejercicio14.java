8a4f97fd4a0adfa302ea5c9bc9605cbc4ddc37db
Sid Ahmed Souidi <Sid Ahmed Souidi@DESKTOP-LDML3OF>
2023-10-10 09:38:03 +0200

    facotrial

---
+++ src/unidad2/ejemplos/Ejercicio14.java
@@ -0,0 +1,26 @@
+package unidad2.ejemplos;
+
+public class Ejercicio14 {
+
+	
+	public static void main(String[] args) {
+		        
+	int numero = 5;
+	int factorial = 4; // Inicializa el resultado del factorial a 1
+	int resultado;
+	int contador = 0 ;
+	for (int i = 0; i <= numero && i <= factorial ;numero = numero -1 ,factorial = factorial = factorial -1 ) {
+		        
+		System.out.println(numero);
+		System.out.println(factorial);
+		resultado = numero * factorial ;
+		contador = ++resultado 
+		//System.out.println(resultado);
+
+	}
+
+		//System.out.println("El factorial de " + numero + " es: " + resultado);
+		    }
+	}
+
+
