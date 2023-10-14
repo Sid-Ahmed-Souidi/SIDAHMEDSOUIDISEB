268d19619934ab80e5d87dd5d2b2b90e8af3449c
Sid Ahmed Souidi <Sid Ahmed Souidi@DESKTOP-LDML3OF>
2023-10-14 17:50:28 +0200

    commit

--- src/unidad2/ejemplos/Ejercicio21.java
+++ src/unidad2/ejemplos/Ejercicio21.java
@@ -14,42 +14,97 @@ public class Ejercicio21 {
 		La aplicación debe ingresar la siguiente información y mostrar el costo del usuario por día de conducción al trabajo:
 		kilometros totales recorridos por día.
 		Costo por litro de gasolina.
-		Promedio de millas por galón.
+		Promedio de kilometros por litro.
 		Tarifas de estacionamiento por día.
 		Peajes por día.*/
 		
-		double kilometroTotal ;
-		double costoGasolina;
-		double gastoConsumo;
-		double estacionamiento;
+		double costoGasolinaxL;
+		double eficiencia;
+		double promedio ;
+		double CostoLxG ; 
+		double numeroDelitros; 
+		double precioEstacionamiento;
+		double tarifa ;
+		double horas ;
+		double horasAminutos;
+		double precioCombustible; 
+		double numeroKilometrosTotal ;
 		double peaje;
-		
-		double gastoTotal ; 
-		
-		double sumaIda ; 
-		double sumaVuelta; 
-		double resultado ;
-		double kilometroTotalV ,costoGasolinaV ,gastoConsumoV,estacionamientoV ,peajeV;
+		int numeroPeajes ;
+		double totalPrecioPeajes ; 
 		Scanner entrada = new Scanner(System.in);
 		
 		
 		//**El consumo por kilometro gasolina debo realizar una operacion 
 		
-		System.out.println("El costo total kilometrorecorriddo ida y vuelta  ");
-		kilometroTotal = entrada.nextDouble();
-		kilometroTotalV = entrada.nextDouble();
-
+		System.out.println("Numero de Kilometros recorridos");
 		
-		System.out.println("El costo total gasolina ida y vuelta");
-		costoGasolina = entrada.nextDouble();
-		costoGasolinaV = entrada.nextDouble();
+		numeroKilometrosTotal = entrada.nextDouble();
+		
+		System.out.println("Precio de combustible por litro");
+		
+		precioCombustible = entrada.nextDouble();
+		
+		System.out.println("Eficiencia de coche en litros por cada 100 kilómetros");
+		
+		eficiencia = entrada.nextDouble();
+		
+		
+		CostoLxG = precioCombustible / eficiencia ; 
+		System.out.println("Costo por litro de gasolina "+CostoLxG);
+		
+		
+		numeroDelitros = numeroKilometrosTotal / eficiencia ; 
+		System.out.println("El numero de litros usados"+numeroDelitros);
+
+		promedio = numeroKilometrosTotal / numeroDelitros ; 
+		System.out.println("El promedio de kilometros por litro"+promedio);
+		
+		
+		System.out.println("Tarifa de estacionamiento  en euros/minutos");
+		
+		tarifa = entrada.nextDouble(); 
+		
+		System.out.println("Numero de Horas estacionados");
+		
+		horas = entrada.nextDouble(); 
+		
+		horasAminutos = horas * 60 ;
+
+		precioEstacionamiento = horasAminutos * tarifa ;  
+		System.out.println("El precio de estacionamiento por dia es de"+precioEstacionamiento);
+		
+		
+		
+		System.out.println("numero de peajes encotrados");
+		numeroPeajes = entrada.nextInt(); 
+		
+		totalPrecioPeajes = 0;
+		for (int i = 1; i <= numeroPeajes; i ++) {
+			System.out.println("El precio de cada peaje");
+			peaje = entrada.nextDouble();
+			
+			totalPrecioPeajes += peaje;
+		}
+			
+		System.out.println("El total del precio de los peajes"+totalPrecioPeajes);
+				
+				
+				
+		entrada.close();
+		
+		
+/**		System.out.println("El costo por litro de gasolina ");
+		costoGasolinaxL = entrada.nextDouble();
+		
+		System.out.println("El promedio de kilometros por litro");
 
 
 		System.out.println("El gasto  gasolina por kilometro recorrido ");
 		gastoConsumo = entrada.nextDouble();
 		gastoConsumoV = entrada.nextDouble();
 		
-		gastoConsumoTotal = 
+		gastoConsumoTotal = (Litros consumidos x 100) /km recorridos
 
 
 		System.out.println("El costo total estacionamiento ida y vuelta ");
@@ -73,7 +128,7 @@ public class Ejercicio21 {
 		
 		System.out.println("El resultado de la media es; "+resultado);
 		
-		
+		*/
 
 		
 
