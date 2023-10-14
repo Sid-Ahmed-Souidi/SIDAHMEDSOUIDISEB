313e4fa9aec2b38d42c1c373b57647a10a49bf00
Sid Ahmed Souidi <Sid Ahmed Souidi@DESKTOP-LDML3OF>
2023-10-14 18:59:31 +0200

    ejercicio22

--- src/unidad2/ejemplos/Ejercicio22Entregar.java
+++ src/unidad2/ejemplos/Ejercicio22Entregar.java
@@ -1,5 +1,7 @@
 package unidad2.ejemplos;
 
+import java.util.Scanner;
+
 public class Ejercicio22Entregar {
 
 	public static void main(String[] args) {
@@ -7,7 +9,48 @@ public class Ejercicio22Entregar {
 		Escriba una aplicación que ingrese estos valores y luego muestra la población mundial estimada después de uno, dos, tres,
 		cuatro y cinco años. */
 		
-		int poblacionMundial
+		long poblacionMundial ;
+		double tasaActual; 
+		double tasaActualDecimal;
+		double previsionPoblacion ; 
+	    double	previsionPoblacion2; 
+	    double previsionPoblacion3;
+	    double previsionPoblacion4;
+	    double previsionPoblacion5;
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("introduzca el numero de la poblacion mundial");
+		
+		poblacionMundial = entrada.nextLong();
+		
+		System.out.println("introduzca el porcentaje de la tasa anual de crecimiento de la población mundial");
+		
+		tasaActual = entrada.nextDouble();
+		
+		
+		//Pasar a decimal el porcentaje de la tasa anual 
+		
+		tasaActualDecimal = tasaActual / 100 ;
+		
+		// realizo el calculo de la prevision 
+		
+		 previsionPoblacion = poblacionMundial + ( poblacionMundial * tasaActualDecimal) ;
+		 System.out.println("Estimacion de la poblacion dentro de un año"+previsionPoblacion);
+		 
+		 
+		 previsionPoblacion2 = previsionPoblacion +( previsionPoblacion * tasaActualDecimal) ;
+		 System.out.println("Estimacion de la poblacion dentro de dos años"+previsionPoblacion2);
+
+		 previsionPoblacion3 = previsionPoblacion2 +( previsionPoblacion2 * tasaActualDecimal) ;
+		 System.out.println("Estimacion de la poblacion dentro de tres años"+previsionPoblacion3);
+
+		 previsionPoblacion4 = previsionPoblacion3 +( previsionPoblacion3 * tasaActualDecimal) ;
+		 System.out.println("Estimacion de la poblacion dentro de cuatro años"+previsionPoblacion4);
+
+		
+		 previsionPoblacion5 = previsionPoblacion4 +( previsionPoblacion4 * tasaActualDecimal) ;
+		 System.out.println("Estimacion de la poblacion dentro de cinco años"+previsionPoblacion5);
 		
 		
 	}
