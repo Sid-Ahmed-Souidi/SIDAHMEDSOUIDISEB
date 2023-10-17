b3c1f32c1629a1ab38816253c49edd2d55517224
ciclofp <ciclofp@10.0.2.15>
2023-10-17 14:12:36 +0200

    fkhi

---
+++ src/unidad2/ejemplos/Ejercicio20.java
@@ -0,0 +1,15 @@
+package unidad2.ejemplos;
+
+public class Ejercicio20 {
+
+	public static void main(String[] args) {
+		Escribe una aplicación que introduzca del usuario el radio de un círculo como un número entero 
+		e imprima el diámetro, la circunferencia y el área del círculo utilizando el valor en coma flotante 3.14159 para π.
+		Utiliza las siguientes fórmulas (r es el radio):
+
+			diámetro = 2r 
+
+			circunferencia = 2πr 
+	}
+
+}
