af63d847d2b971983ed78f934bd988a98e0afb65
Sid Ahmed Souidi <Sid Ahmed Souidi@DESKTOP-LDML3OF>
2023-10-19 10:23:24 +0200

    capicua

--- src/unidad3/ejemplos/Ejercicio8capicua.java
+++ src/unidad3/ejemplos/Ejercicio8capicua.java
@@ -9,7 +9,11 @@ public class Ejercicio8capicua {
 	Scanner entrada = new Scanner(System.in);
 		
 		int numero ;
-		int modulo;
+		int ultimoDigito;
+		int penultimoDigito ; 
+		int antepenultimo ;
+		int primerDigito ; 
+		int segundoDigito ; 
 		
 		System.out.println("Introduzca un numero del 0 al 99.999");
 		numero = entrada.nextInt();
@@ -19,17 +23,52 @@ public class Ejercicio8capicua {
 		
 			
 		}else if(numero >= 10 && numero <= 99) {
-			System.out.println("El numero que ha introducido es de dos cifras");
-			modulo = numero / 10
+			ultimoDigito = numero % 10;
+			penultimoDigito = numero / 10 ;
+			if(ultimoDigito==penultimoDigito) {
+				System.out.println("El numero  es capiúa");
+			}else{
+				System.out.println("El numero no es capicúa");
+			
+			}
 		}else if(numero >= 100 && numero <= 999) {
 			System.out.println("El numero que ha introducido es de tres cifras");		
+			ultimoDigito = numero % 10;
+			antepenultimo = numero / 100;
+			if(ultimoDigito==antepenultimo) {
+				System.out.println("El numero es capicúa");
+			}else {
+				System.out.println("El numero no es capicúa");
+			}
 			
 		}else if(numero >= 1000 && numero <= 9999) {
 			System.out.println("El numero que ha introducido es de cuatro cifras");		
-			
+			ultimoDigito = numero % 10;
+			penultimoDigito = (numero / 10) % 10  ; //saco el ultimo digito ej (1234 a 123)y hago el modulo con 123 (que obtendria el el ultimo de 123 que es 3)
+			antepenultimo = (numero / 100) % 10 ; // dividiendo entre 100 obtengo de (1234 a 12 ) y luego hago modelo de ese numero(12) que es 2 el antepenultimo
+			primerDigito = numero / 1000;
+			System.out.println("f"+ultimoDigito+"ddfa"+penultimoDigito+"asdf"+antepenultimo+"gfd"+primerDigito);
+			if(primerDigito==ultimoDigito && penultimoDigito==antepenultimo) {
+				System.out.println("El numero es capicúa");
+			}else {
+				System.out.println("El numero no es capicúa");
+			}
+
+
 		}else if(numero >= 10000 && numero <= 99999) {
 			System.out.println("El numero que ha introducido es de cinco cifras");		
+			ultimoDigito = numero % 10;
+			penultimoDigito = (numero / 10) % 10  ; //saco el ultimo digito ej (1234 a 123)y hago el modulo con 123 (que obtendria el el ultimo de 123 que es 3)
+			antepenultimo = (numero / 100) % 10 ; // dividiendo entre 100 obtengo de (1234 a 12 ) y luego hago modelo de ese numero(12) que es 2 el antepenultimo
+			primerDigito = (numero / 1000) / 10; // divido ej(12345) entre 1000 obtengo 12 y divido otra vez por 10 para quedarme con el primero que es 1 
+			segundoDigito = (numero / 1000) % 10; // dividimos ej(12345) entre 1000 y nos quedamos con 12 y con 12 realizamos modulo y me quedo con el segundoDigito 
 			
+			System.out.println("f"+ultimoDigito+"ddfa"+penultimoDigito+"asdf"+antepenultimo+"gfd"+segundoDigito+"asdf"+primerDigito);
+			if(primerDigito==ultimoDigito && segundoDigito==penultimoDigito) {
+				System.out.println("El numero es capicúa");
+			}else {
+				System.out.println("El numero no es capicúa");
+			}
 		}
 		
 
--- src/unidad3/ejemplos/PracticaCapicua.java
+++ src/unidad3/ejemplos/PracticaCapicua.java
@@ -4,10 +4,19 @@ public class PracticaCapicua {
 
 	public static void main(String[] args) {
 
-		int numero = 87;
-		int ultimoDigito = numero % 10;
-		int penultinmoDigito = numero 
-		System.out.println("El último dígito de " + numero + " es " + ultimoDigito);
+        int numero = 87;
+        int numero2 = 934 ;
+        int numero3 = 6456;
+        int ultimoDigito = numero % 10;
+        int penultimoDigito = (numero / 10) % 10;
+        int antepenultimo = (numero2 / 100);
+        int primero = numero3 / 1000;
+
+        System.out.println("El último dígito de " + numero + " es " + ultimoDigito);
+        System.out.println("El penúltimo dígito de " + numero + " es " + penultimoDigito);
+        System.out.println("El penúltimo dígito de " + numero + " es " + antepenultimo);
+        System.out.println("El penúltimo dígito de " + numero + " es " + primero);
+
 	}
 
 }
