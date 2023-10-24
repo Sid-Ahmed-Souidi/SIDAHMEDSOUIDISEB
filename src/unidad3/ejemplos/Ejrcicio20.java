9208b05f625bcfba5bdc994ab8d195daabc61590
ciclofp <ciclofp@10.0.2.15>
2023-10-24 12:46:46 +0200

    Placas Solares

--- src/unidad3/ejemplos/Ejrcicio20.java
+++ src/unidad3/ejemplos/Ejrcicio20.java
@@ -1,17 +1,96 @@
 package unidad3.ejemplos;
 
+import java.util.Scanner;
+
 public class Ejrcicio20 {
 
 	public static void main(String[] args) {
-	
-		int cuadrado ,lado;
-		double pi
-		int circulo ;
-		int rectangulo;
-		int trapecio;
-		int triangulo;
+		Scanner entrada = new Scanner(System.in);
 		
-		cuadrado = 
+		double cuadrado , lado = 0 ;
+		double pi= 3.14;
+		double circulo , radio = 0;
+		double rectangulo;
+		double trapecio , base1 = 0 , base2 = 0;
+		double triangulo, base = 0 , altura = 0;
+		int seleccion ; 
+		
+		cuadrado = lado * lado; 
+		circulo = pi * radio * radio ;
+		rectangulo = base * altura;
+		trapecio = (base1+ base2) * altura / 2;
+		triangulo = (base * altura) / 2;
+		
+		do {
+			
+			System.out.println("CALCULO DE SUPERFICIES");
+			System.out.println("1.Cuadrado lado*lado");
+			System.out.println("2.Círculo pi*radio*radio");
+			System.out.println("3.Rectángulo base*altura");
+			System.out.println("4.Trapecio (base1+base2)*altura/2");
+			System.out.println("5.Triángulo (base*altura)/2");
+			System.out.println("6.Salir");
+			seleccion = entrada.nextInt();
+			
+			if(seleccion == 1) {
+				System.out.println("Calculo del cuadrado");
+				System.out.println("Introduzca el valor de lado del cuadrado");
+				lado = entrada.nextDouble();
+
+				cuadrado = lado * lado; 
+				
+				System.out.println("Resultado :"+cuadrado);
+				
+			}else if(seleccion == 2) {
+				System.out.println("Calculo del Circulo");
+				System.out.println("Introduzca el valor de radio");
+				radio = entrada.nextDouble();
+				System.out.println("Introduzca el valor de la altura");
+				circulo = pi * radio * radio ;
+				System.out.println("Resultado :"+circulo);
+			
+			}else if(seleccion == 3) {
+					System.out.println("Calculo del rectangulo");
+					System.out.println("Introduzca el valor de la base");
+					base = entrada.nextDouble();
+					System.out.println("Introduzca el valor de la altura");
+					altura = entrada.nextDouble();
+					rectangulo = base * altura;
+
+					System.out.println("Resultado :"+rectangulo);
+				
+			}else if(seleccion == 4) {
+						System.out.println("Calculo del trapecio");
+						System.out.println("Introduzca el valor de la base1");
+						base = entrada.nextDouble();
+						System.out.println("Introduzca el valor de la base2");
+						altura = entrada.nextDouble();
+						System.out.println("Introduzca el valor de la altura");
+						altura = entrada.nextDouble();
+						trapecio = (base1+ base2) * altura / 2;
+
+						System.out.println("Resultado :"+trapecio);
+					
+			}else if(seleccion == 5) {
+							System.out.println("Calculo del triangulo");
+							System.out.println("Introduzca el valor de la base");
+							base = entrada.nextDouble();
+							System.out.println("Introduzca el valor de la altura");
+							altura = entrada.nextDouble();
+							triangulo = (base * altura) / 2;
+							System.out.println("Resultado :"+triangulo);
+			
+			
+			}else if(seleccion== 6) {
+					System.out.println("HASTA LUEGO .");
+					
+			}
+			
+			
+			
+		}while(seleccion!=6);
+	
+		
 		
 		
 		
---
+++ src/unidad3/ejemplos/PlacasSolares.java
@@ -0,0 +1,91 @@
+package unidad3.ejemplos;
+
+import java.util.Scanner;
+
+public class PlacasSolares {
+
+	public static void main(String[] args) {
+	Scanner entrada = new Scanner(System.in);
+	double energiaGenerada = 0 ;
+	String ide = null ;
+	double latitud = 0 , longitud = 0; 
+	boolean estado = true;
+	int seleccion ; 
+	double calcularDia ; 
+	int dias ; 
+	
+	
+	do {
+		System.out.println("1.Registrar un panel solar ");
+		System.out.println("2.Consultar informacion del panel solar");
+		System.out.println("3.Cambiar el estado de un panel solar ");
+		System.out.println("4.Calcular la generacion diaria total de energia");
+		System.out.println("5.Salir del programa");
+		seleccion = entrada.nextInt();
+		
+		if(seleccion == 1 ) {
+			System.out.println("Ingresa la cantidad de energia generada");
+			energiaGenerada = entrada.nextDouble();
+			System.out.println("Ingrese la ubicacion de la latitud");
+			latitud = entrada.nextDouble();
+			System.out.println("Ingrese la ubicacion de la longitud");
+			longitud = entrada.nextDouble();
+			System.out.println("Ingrese la identificacion");
+			ide = entrada.next();
+
+			
+		}else if(seleccion == 2) {
+			
+			if(energiaGenerada==0 && ide==null && latitud==0 &&longitud ==0 ) {
+				System.out.println("Debe ingresar datos en la seleccion 1");}
+			else {	
+				System.out.println("informacion de la energia generada"+energiaGenerada);
+				System.out.println("informacion de la latitud"+latitud);
+				System.out.println("informacion de la longitud"+longitud);
+				System.out.println("informacion de la identificacion"+ide);
+				System.out.println("Informacion del estado"+estado);}
+			
+
+			
+		}else if(seleccion == 3) {
+			System.out.println("Estado actual activo"+estado);
+			System.out.println("Ha seleccionado cambiar el estado a inactivo");
+			estado = false;
+			System.out.println("Estado inactivo "+estado);
+			
+			
+			
+			
+		}else if(seleccion ==4) {
+		System.out.println("Calcular energia segun el numero de dias");
+		dias = entrada.nextInt();
+		calcularDia = dias * energiaGenerada ; 
+		System.out.println("Este es la generacion de energia total de estos dias"+calcularDia);
+		
+			
+		}else if(seleccion ==5) {
+			System.out.println("Su opcion seleccionada es salir del programa.Hasta la proxima");
+			
+		}
+		
+			
+			
+			
+			
+		
+		
+
+
+		
+		
+		
+		
+		
+	}while(seleccion!=5);
+	
+		
+		
+	}
+
+	
+}
