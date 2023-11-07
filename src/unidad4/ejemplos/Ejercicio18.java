78fcf7f810d3e54772c4f2c8395bb2125429c9b7
Sid Ahmed Souidi <Sid Ahmed Souidi@DESKTOP-LDML3OF>
2023-11-07 20:51:40 +0100

    zdfxvcb

---
+++ src/unidad4/ejemplos/Ejercicio18.java
@@ -0,0 +1,25 @@
+package unidad4.ejemplos;
+
+import java.util.Scanner;
+
+public class Ejercicio18 {
+	
+	public static double quintosAEuros(int numero) {
+		double euros ;
+		euros = numero * 0.20 ;
+		return euros ;
+		
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Introduzca el numero de monedas de 20 centimos");
+		int numero = entrada.nextInt();
+		System.out.println("este es la equivalencia : "+quintosAEuros(numero)+" euros");
+		
+		
+		
+	}
+
+}
