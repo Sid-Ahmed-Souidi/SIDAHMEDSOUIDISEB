5513b0cefc0d30afdbf949abda8d2dd464c33be0
ciclofp <ciclofp@10.0.2.15>
2023-11-09 14:28:55 +0100

    mamak

---
+++ src/unidad4/ejemplos/CalularFactorial.java
@@ -0,0 +1,28 @@
+package unidad4.ejemplos;
+
+public class CalularFactorial {
+
+	public static void main(String[] args) {
+		int numero = 6;
+		int resultado = calcularFactorial(numero);
+		System.out.println("El factorial de "+numero+"es: "+resultado);
+				
+		
+	}
+
+	
+	
+	
+	private static int calcularFactorial(int numero) {
+		int resultado;
+		if(numero==0 || numero==1) {
+			resultado=1;
+			
+		}else {
+			resultado = numero * calcularFactorial(numero-1);
+	
+		}return resultado;
+		
+	}
+	
+}
