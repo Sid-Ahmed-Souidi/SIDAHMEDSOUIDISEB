9a9a5c1d11d1b9e114494dd6360445af0e1d136b
Sid Ahmed Souidi <Sid Ahmed Souidi@DESKTOP-LDML3OF>
2023-11-21 22:01:28 +0100

    terminar el 28

--- src/unidad4/ejemplos/Ejercicio28.java
+++ src/unidad4/ejemplos/Ejercicio28.java
@@ -1,49 +1,78 @@
 package unidad4.ejemplos;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Ejercicio28 {
 	
 	static int nHabitaciones ;
-	static int pPinturaKg ;
-	static int mCuadrados ; 
-	static int totalMCuadrados ; 
+	static double pPinturaKg ;
+	static double mCuadrados ; 
+	static double totalMCuadrados ; 
 	static double cantidadPintura;
-	static int horasNecesarias ; 
+	static double costoPintura;
+	static double cargosLaborales;
+	static double horasNecesarias ; 
+	static double costoTotal ; 
 	static final int  cobroXhora  = 14;
+	
+	static DecimalFormat df = new DecimalFormat("0.00##");
+
 	static Scanner entrada = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		
 		pedirDatos();
-		System.out.println(mCuadrados());	
-		System.out.println(cantidadPitura()); 
-		System.out.println(horasTabajo()); 
-		
-		
-		
+		System.out.println("Metros cuadrados "+mCuadrados());	
+		System.out.println("Horas de trabajo son :"+horasTabajo());
+		System.out.println("El costo total de pintura son de"+costoDePintura());
+	    System.out.println("Los cargos laborales son de "+cargosLaborales());  
+	   System.out.println("El costo total de la pintura es de:"+costoTotal()); 
 		
 	}
 
-	private static int  horasTabajo() {
-		for(int i = 35 ;   i <= totalMCuadrados ; i ++) {
-			horasNecesarias += 8 ;
+	
+
+	private static double costoTotal() {
+		
+		costoTotal = costoPintura + cargosLaborales ;
+		return costoTotal;
+	}
+
+
+
+	private static double cargosLaborales() {
+		cargosLaborales = horasNecesarias * 14.00;
+		df.format(cargosLaborales);
+		return cargosLaborales;
+	}
+
+
+
+	private static double  horasTabajo() {
 			
-		}return horasNecesarias;
-		
+			horasNecesarias =  (totalMCuadrados / 35) * 8;
+			df.format(cargosLaborales);
+			return horasNecesarias;	
 	}
-
+	private static double costoDePintura() {
+		costoPintura = cantidadPintura * pPinturaKg;
+		df.format(costoPintura);
+		return costoPintura;
+	}
 	private static double cantidadPitura() {
 		
-		cantidadPintura = totalMCuadrados / 35 ;
+		cantidadPintura = (totalMCuadrados / 35) * 3.7 ;
+		
 		return cantidadPintura ; 
 		
 	}
+	
 
-	private static int  mCuadrados() {
+	private static double  mCuadrados() {
 		System.out.println("Introduzca los metros cuadrados por habitacion");
 		for(int i = 0 ; i < nHabitaciones ; i ++) {
-			System.out.println("la "+i+1+ "habitacion");
+			System.out.println("la "+(i+1)+ "habitacion");
 			mCuadrados = entrada.nextInt();
 			totalMCuadrados += mCuadrados ;
 		}return totalMCuadrados ;
@@ -54,7 +83,7 @@ public class Ejercicio28 {
 		System.out.println("Introduce el numero de habitaciones a pintar");
 		nHabitaciones = entrada.nextInt();
 		System.out.println("Introduce el precio de pintura por Kg");
-		pPinturaKg = entrada.nextInt();
+		pPinturaKg = entrada.nextDouble();
 
 	}
 	
