96991683d953fed56a860bbe1de59d40cf4cbbe7
ciclofp <ciclofp@10.0.2.15>
2023-11-30 14:29:25 +0100

    sasdf

--- src/unidad5/ejemplos/Mancala.java
+++ src/unidad5/ejemplos/Mancala.java
@@ -2,145 +2,104 @@ package unidad5.ejemplos;
 
 import java.util.Scanner;
 
-import javax.annotation.processing.SupportedSourceVersion;
-
 public class Mancala {
 
 	
-	static final int NUM_FILAS = 2;
-	static final int NUM_COLUMNAS = 8;
 	static final String usuario1 = "Pepe";
 	static final String usuario2 = "Lola";
-	static int[][] juego = new int[NUM_FILAS][NUM_COLUMNAS];
+	static final int NC=8;
+	static final int NF=2;
+	static int[][] tablero =  new int[NF][NC];
 	static Scanner entrada = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		
-		
-		iniciarTablero();
+	
+		inicializarTablero();
 		imprimirTablero();
 		boolean finJuego = false;
 		int veces = 0;
 		String jugadorSiguiente=usuario1;
-	do {
-		if(veces< 6) {
-			if(jugadorSiguiente==usuario1) {
-				pedirJugada(usuario1);
-				jugadorSiguiente=usuario2;
-				imprimirTablero();
+		do {
+			if(veces<6) {
+				if(jugadorSiguiente==usuario1) {
+					pedirJugada(usuario1);
+					jugadorSiguiente=usuario2;
+					imprimirTablero();
 				}else {
 					pedirJugada(usuario2);
 					jugadorSiguiente=usuario1;
-				}		
-		}else {
+					imprimirTablero();
+				}
+			}else {
+				finJuego=true;
+			}
+			veces++;
 			
-			finJuego=true;
-		}
-		veces++;
-		
-		
-	}while(!finJuego);	
-		
-		
+		}while(!finJuego);
+
 	}
 
-	
-	
-	
-	
-	
 	private static void pedirJugada(String jugador) {
-		System.out.println(jugador+"- Deme una casilla: ");
-		boolean coordenadaCorrecta = false;
-		int fila = 0;
 		
-		fila = pediCoordenada(jugador, fila, fila);
-		System.out.println("Fila(1-2):");
-
-		do {
-			fila = entrada.nextInt();
-			if(fila < 1 || fila >2) {
-				System.out.println("Error en la fila .Deme otra coordenada");
-			}else {
-				coordenadaCorrecta=true;
-			}
-			
-			
-		}while(!coordenadaCorrecta);
 		
-		coordenadaCorrecta = true;
+		System.out.println(jugador+"- Deme una casilla:");
 		
-		int columna = 0;
-		System.out.println("Columna(1-8):");
-		do {
-			columna = entrada.nextInt();
-			if(columna < 1 || columna >8) {
-				System.out.println("Error en la fila .Deme otra coordenada(1-8) : ");
-			}else {
-				coordenadaCorrecta=true;
-			}
-			
-			
-		}while(!coordenadaCorrecta);
-	
+		int fila=0;
+		int columna=0;
+		
+		fila = pedirCoordenada("fila",1,NF);
+		columna = pedirCoordenada("columna", 1, NC);
+		
 		obtenerContenidoCasilla(fila,columna);
 		
 		
-		
-		
 	}
 
-	private static int pediCoordenada(String nombre , int limiteInferior , int limiteSuperior) {
+	private static int pedirCoordenada(String nombre, int limiteInferior, int limiteSuperior) {
 		
-		int coordenada= 0;
+		int coordenada=0;
+		boolean coordenadaCorrecta=false;
 		System.out.println(nombre+"("+limiteInferior+"-"+limiteSuperior+")");
-		do{
-			
-			coordenada = entrada.nextInt();
-			if(coordenada < limiteInferior || coordenada>limiteSuperior) {
-				System.out.println("Error en la "+nombre+".Deme otra coordenada("+limiteInferior+limiteSuperior);
+		do {
+			coordenada =  entrada.nextInt();
+			if(coordenada<limiteInferior||coordenada>limiteSuperior) {
+				System.out.println("Error en la "+nombre+". Deme otra coordenada("+limiteInferior+"-"+limiteSuperior+"):");
 				
+		}else {
+				coordenadaCorrecta=true;
 			}
-			
-			
-		}
-		
-		
+		}while(!coordenadaCorrecta);
+		return coordenada;
 	}
-
-
-
-
-
 
 	private static void obtenerContenidoCasilla(int fila, int columna) {
 		
+		System.out.println("Contenido:"+tablero[fila-1][columna-1]);
+		tablero[fila-1][columna-1]=0;
 		
 	}
-
-
-
-
-
 
 	private static void imprimirTablero() {
-		for(int i = 0 ; i < NUM_FILAS ; i++) {
-			for(int j = 0 ; i < NUM_COLUMNAS ; j++) {
-				System.out.println(" | " + juego[i][j]+" | ");
-			
+		for(int i=0;i<NF;i++) {
+			for(int j=0;j<NC;j++) {
+				if((i==1&&j==0) || (i==0&&j==NC-1)) {
+					System.out.print(" |");
+				}else {
+					System.out.print(tablero[i][j]+"|");
+				}
+				
 			}
 			System.out.println("");
-	}
-	}
-	private static void iniciarTablero() {
+		}
 		
-		for(int i = 0 ; i < NUM_FILAS ; i++) {
-			for(int j = 0 ; i < NUM_COLUMNAS -1; j++) {
-				juego[i][j] = 4;
-				
-				
+	}
+
+	private static void inicializarTablero() {
+		
+		for(int i=0;i<NF;i++) {
+			for(int j=1;j<NC-1;j++) {
+				tablero[i][j]=4;
 			}
-			
 		}
 		
 	}
