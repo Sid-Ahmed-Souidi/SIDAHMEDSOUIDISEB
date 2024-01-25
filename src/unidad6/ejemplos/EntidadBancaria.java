134c3d5b8d1c505bf6174cc2208739ed94410bcf
Sid Ahmed Souidi <Sid Ahmed Souidi@DESKTOP-LDML3OF>
2024-01-25 10:44:39 +0100

    terminar entidad bancaria

--- src/unidad6/ejemplos/EntidadBancaria.java
+++ src/unidad6/ejemplos/EntidadBancaria.java
@@ -1,11 +1,12 @@
 package unidad6.ejemplos;
 
+import java.lang.reflect.Array;
 import java.util.Random;
 import java.util.Scanner;
 
 public class EntidadBancaria {
 
-	public static int[] ocultar1 = new int[5];
+	public static String[] ocultar1 ;
 	public static int[] matriz = {1,2,3,4,5};
 	public static final String dni = "12345678V";
 	public static final String tarjetaBancaria = "1111222233334444";
@@ -19,24 +20,26 @@ public class EntidadBancaria {
 		
 		
 		int numero1 = random.nextInt(9) +0;
-		System.out.println("N1  ="+numero1);
 		int numero2 = random.nextInt(9) +0;
-		System.out.println("n2 ="+numero2);
-		if(numero1 != numero2) {
-			
-			
-		}
-		if(numero1 == numero2) {
-			numero2 = random.nextInt(9) +0;
-			System.out.println(" n2if ="+numero2);
-		}
 		
-		 int numero3 = random.nextInt(9) +0;
-		 System.out.println("n3 ="+numero3);
-		 if(numero2 == numero3 || numero1 == numero3) {
-			 numero3 = random.nextInt(9) +0;
-			 System.out.println("n3if ="+numero3);
-		 }
+		//  el bucle while seguira generando mientras el numero 1 y numero 2 sean iguales
+			while(numero1 == numero2) {
+			numero2 = random.nextInt(9) +0;
+			
+		}
+			int numero3 = random.nextInt(9) +0;
+			
+		// el bucle while seguira generando mientras el numero3 sea igual a numero1 o numero3 sea igual a numero2 
+			while(numero3== numero1 || numero3 ==numero2) {
+				numero3 = random.nextInt(9) +0;
+			}
+			
+		System.out.println(numero1);
+		System.out.println(numero2);
+		System.out.println(numero3);
+		
+
+		claveSeguridad();
 		inicioSesion();
 		
 	}
@@ -76,14 +79,28 @@ public class EntidadBancaria {
 		
 	}
 	private static void claveSeguridad() {
-		int numero1 = random.nextInt(9) +0;
+		int numero1 = random.nextInt(4) +0;
 		int numero2 = random.nextInt(9) +0;
-		do {
-		numero2 = random.nextInt(9) +0;
-		}while(numero1 != numero2);
-		int numero3 = random.nextInt(9) +0;
-		
-		
+		//  el bucle while seguira generando mientras el numero 1 y numero 2 sean iguales
+			while(numero1 == numero2) {
+			numero2 = random.nextInt(9) +0;
+		}
+			int numero3 = random.nextInt(9) +0;
+		// el bucle while seguira generando mientras el numero3 sea igual a numero1 o numero3 sea igual a numero2 
+			while(numero3== numero1 || numero3 ==numero2) {
+				numero3 = random.nextInt(9) +0;
+			}
+			
+			for(int i = 0 ; i < matriz.length ; i ++) {
+				 if(matriz[i] == numero1) {
+					 System.out.print("X1");
+				 }else {
+					 System.out.print(matriz[i]);
+					 
+				 }
+				 System.out.print(" | ");
+				
+			}
 	
 	}
 
