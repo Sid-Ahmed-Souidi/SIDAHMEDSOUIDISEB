51b8627613e0d00c59daecde4f6538034e019b6d
Sid Ahmed Souidi <Sid Ahmed Souidi@DESKTOP-LDML3OF>
2024-02-19 16:40:07 +0100

    Merge branch 'master' of https://github.com/Sid-Ahmed-Souidi/SIDAHMEDSOUIDISEB.git

--- src/Pruebas1/Ejercicio1Prueba1.java
+++ src/Pruebas1/Ejercicio1Prueba1.java
@@ -9,51 +9,78 @@ public class Ejercicio1Prueba1 {
 	public static int[] pila = new int[tam];
 	public static int[] cola = new int[tam];
 	public static int seleccion ;
+	public static int contador ;
+	public static int contador0 ;
+
 	public static void main(String[] args) {
 		
 		do {
 			System.out.println("1. Añadir en pila");
-			System.out.println("2. salir");
+			System.out.println("2.Extraer de la pila");
+			System.out.println(". salir");
 			seleccion = entrada.nextInt();
 			switch(seleccion) {
 			
 			case 1:
 				añadirPila();
 				break;
+			case 2:
+				extraerPila();
+				break;
+			
 			default:
 				System.out.println("Seleccione una de las opciones");
 			}
-		
-		
-		
-		}while(seleccion!=2);
+		}while(seleccion!=3);
 	}
+	private static void extraerPila() {
+		boolean extraido = false ;
+		for(int i = 0 ; i < pila.length && !extraido ; i++) {
+			if(pila[i] != 0) {
+			pila[i] =0;
+			System.out.println("Elemento extradido");
+			extraido = true;
+			mostrarArray();
+			}
+		}
+		
+		// terminar la comprobacion de que si la fila esta vacia mandar un mensaje 
+		for(int j = 0 ; j < pila.length ; j++) {
+			if(pila[j] == 0) {
+				contador++;
+				
 
+			}
+			
+		}
+					
+				
+			
+		
+	}
 	private static void añadirPila() {
-		boolean introducido = false;
-		boolean movido = false;
 		System.out.println("Introduce el numero en la pila");
 		int numero = entrada.nextInt();
-		for(int i = 0 ; i < tam && !introducido ; i++ ) {
-			if(pila[i] == 0) {
-				pila[i] = numero;
-				introducido = true;
-			}
+		if(contador== pila.length) {
+			System.out.println("La cola esta llena");
 		}
-		for(int i = pila.length -1  ; i >= 0; i--) {			
-				pila[i] = pila[i-1];
-				pila[i] = 0;
-				
-				
+		else {
+		for(int i = pila.length -1  ; i > 0; i--) {	
+				pila[i] = pila[i-1];// todos los elementos se desplazan a la derecha
+				//System.out.println(i+"-"+(i-1) );
 			}
-			
+			pila[0] = numero;
+			contador++;
+		System.out.println("Contador "+contador);
 		}
+		mostrarArray();
 		
+	}
+
+	private static void mostrarArray() {
 		for( int numero1 : pila) {
 			System.out.print(numero1+" | ");
-			
-		}
-		
+		}System.out.println();		
 	}
 
 }
