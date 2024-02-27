1d077c550738d17829f36d62cba9c1da97969546
diurno <diurno@equipo-00>
2024-02-27 14:28:52 +0100

    ajedrez

--- src/unidad5/ejemplos/ampliacion/Ajedrez.java
+++ src/unidad5/ejemplos/ampliacion/Ajedrez.java
@@ -5,22 +5,35 @@ public class Ajedrez {
 	
 	public static final int filas = 8;
 	public static final int columnas = 8;
-	public static int[][] ajedrez = new int[filas][columnas];
+	public static int[][] ajedrez ;
 	public static void main(String[] args) {
 		
-		posiciones(4,6);
+	ajedrez= new int[filas][columnas];
+			
 		
-	
+		posicionesPositivo(4,6);
+		
 		
 	}
-	private static void posiciones(int pos1 , int pos2) {
-		for(int i = 0 ; i < filas ; i++) {
-			for(int j = 0 ; j < columnas ;j++) {
-				
-				
-			}
+	
+	private static void posicionesPositivo(int pos1 , int pos2) {
+
+		for(int i = pos1 ; (i <ajedrez.length && i< ajedrez.length) ; i++) {
+			ajedrez[i][pos2] = 1;
+			//System.out.println(ajedrez[i][pos2]);
+			pos2++;
+			
 		}
-		
+		mostrarArray();
+	}
+	private static void mostrarArray() {
+		for(int i = 0 ; i < filas ; i++) {
+			for(int j = 0 ; j < columnas ; j++) {
+				System.out.print(ajedrez[i][j]+" ");
+				
+			} 
+			System.out.println();
+		}
 	}
 
 }
